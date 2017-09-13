c78a660eee6df5272ae5f75cc60b4c0621c908cb
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-13 11:05:01 +0200

    Add, Divide, Subtract og Multiply metoder til interface.
    CalculatorService Klassen er derefter opdateret med metoderne

---
+++ WcfServiceCalculator/WcfServiceCalculator/Service1.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : ICalculatorService
+    {
+        
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetAdd()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDivide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMultiply()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSubtract()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
